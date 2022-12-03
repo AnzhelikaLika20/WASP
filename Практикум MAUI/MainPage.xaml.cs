033041756163b17b360d39c68c7e0f49931275ed
Гакал Анжелика <118559583+AnzhelikaLika20@users.noreply.github.com>
2022-12-03 21:06:03 +0300

    Add files via upload

---
+++ Практикум MAUI/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace Tester;
+
+public partial class MainPage : ContentPage
+{
+	static string s = "";
+	string password = "1873";
+	bool flag = true;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+	public void DigitClicked(object sender, EventArgs e)
+	{
+		if(flag)
+		{
+            s += (sender as Button).Text;
+            DisplayLabel.Text = s;
+        }
+    }
+	public void AccessClicked(object sender, EventArgs e)
+	{
+		if (s == password)
+		{
+			flag = false;
+            DisplayLabel.Text = "Success";
+			return;
+        }
+        else
+		{
+           DisplayLabel.Text = "";
+			s = "";
+        }
+    }
+	public void DeleteClicked(object sender, EventArgs e)
+	{
+		if(flag)
+		{
+            s = s.Substring(0, s.Length - 1);
+            DisplayLabel.Text = s;
+        }
+    }
+}
+
